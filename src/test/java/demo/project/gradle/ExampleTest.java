aa37e9c2eed6ea1eb4f23456aa733de006de4158
Matti Wilhelmi <maw@hbt.de>
2022-06-22 08:01:17 +0200

    Empty commit

--- src/test/java/demo/project/gradle/ExampleTest.java
+++ src/test/java/demo/project/gradle/ExampleTest.java
@@ -10,6 +10,7 @@ public class ExampleTest {
    public void test() {
       final ExampleClass exampleClazz = new ExampleClass();
       exampleClazz.calleeMethod(int errorInt);
+
       assertNull(exampleClazz);
    }
 
