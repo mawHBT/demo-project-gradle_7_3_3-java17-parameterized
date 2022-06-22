bc70afae1c29c5341fd56ebdd3e8710c4638fd7d
Matti Wilhelmi <maw@hbt.de>
2022-06-22 07:45:09 +0200

    Added testerror for non-parameterized test

--- src/test/java/demo/project/gradle/ExampleTest.java
+++ src/test/java/demo/project/gradle/ExampleTest.java
@@ -1,21 +1,16 @@
 package demo.project.gradle;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.api.Test;
 
-@DisplayName("Pass the method parameters provided by the @ValueSource annotation")
 public class ExampleTest {
 
-   @DisplayName("Should pass a non-null message to our test method")
-   @ParameterizedTest(name = "index: {index}, message: {0}")
-   @ValueSource(strings = { "Hello", "World" })
-   public void test(final String message) {
+   @Test
+   public void test() {
       final ExampleClass exampleClazz = new ExampleClass();
-      exampleClazz.calleeMethod(message);
-      assertNotNull(exampleClazz);
+      exampleClazz.calleeMethod("This is a Test");
+      assertNull(exampleClazz);
    }
 
 }
