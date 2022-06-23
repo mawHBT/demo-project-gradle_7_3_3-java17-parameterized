684f51bf37d2aa2a0e2c6e05640ed8819dab5920
Matti Wilhelmi <maw@hbt.de>
2022-06-23 09:55:52 +0200

    Added assert-error and compile-error

--- src/test/java/demo/project/gradle/ExampleTest.java
+++ src/test/java/demo/project/gradle/ExampleTest.java
@@ -1,6 +1,6 @@
 package demo.project.gradle;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,8 +9,8 @@ public class ExampleTest {
    @Test
    public void test() {
       final ExampleClass exampleClazz = new ExampleClass();
-      exampleClazz.calleeMethod();
-      assertNotNull(exampleClazz);
+      exampleClazz.calleeMethod().toString;
+      assertNull(exampleClazz);
    }
 
 }
