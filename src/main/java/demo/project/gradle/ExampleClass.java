cefd70e83627f4cd962846b8a70dcf7746b47d81
Matti Wilhelmi <maw@hbt.de>
2022-06-23 15:43:34 +0200

    Added compileError

--- src/main/java/demo/project/gradle/ExampleClass.java
+++ src/main/java/demo/project/gradle/ExampleClass.java
@@ -3,7 +3,7 @@ package demo.project.gradle;
 public class ExampleClass {
 
    public void calleeMethod(final String message) {
-      new Callee().method1(message);
+      new Callee().method1(message).toString;
    }
 
 }
